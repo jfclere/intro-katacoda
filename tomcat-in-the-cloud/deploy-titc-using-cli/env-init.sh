e49a802dd041335a6969c6b4f5ea21951cea1443
Maxime Beck <mabeck@dhcp-144-193.gva.redhat.com>
2017-09-20 12:38:03 +0200

    Install dependencies and build

--- tomcat-in-the-cloud/deploy-titc-using-cli/env-init.sh
+++ tomcat-in-the-cloud/deploy-titc-using-cli/env-init.sh
@@ -1,1 +1,11 @@
-git clone https://github.com/iSma/tomcat-in-the-cloud
+# Install jdk
+yum install java-1.8.0-openjdk
+export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk
+
+# Installing maven
+wget http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
+tar xvf apache-maven-3.5.0-bin.tar.gz
+mv apache-maven-3.5.0  /usr/local/apache-maven
+export M2_HOME=/usr/local/apache-maven
+export M2=$M2_HOME/bin 
+export PATH=$M2:$PATH
--- tomcat-in-the-cloud/deploy-titc-using-cli/set-env.sh
+++ tomcat-in-the-cloud/deploy-titc-using-cli/set-env.sh
@@ -1,1 +1,6 @@
 ~/.launch.sh
+git clone https://github.com/iSma/tomcat-in-the-cloud
+cd tomcat-in-the-cloud
+oc new-project tomcat-in-the-cloud
+oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default -n $(oc project -q)
+mvn fabric8:deploy

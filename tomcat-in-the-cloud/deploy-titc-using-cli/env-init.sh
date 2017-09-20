bfbd89dea09cc438204538b81ca3d1f5c1a60441
Maxime Beck <mabeck@dhcp-144-193.gva.redhat.com>
2017-09-20 12:44:01 +0200

    Update env-init

--- tomcat-in-the-cloud/deploy-titc-using-cli/env-init.sh
+++ tomcat-in-the-cloud/deploy-titc-using-cli/env-init.sh
@@ -1,5 +1,6 @@
 # Install jdk
 yum install java-1.8.0-openjdk
+y
 export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk
 
 # Installing maven
@@ -9,3 +10,4 @@ mv apache-maven-3.5.0  /usr/local/apache-maven
 export M2_HOME=/usr/local/apache-maven
 export M2=$M2_HOME/bin 
 export PATH=$M2:$PATH
+

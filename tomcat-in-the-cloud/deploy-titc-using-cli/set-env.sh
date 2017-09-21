5a76fb1149c479b41ee2942adaa5f61d67dbcfca
Jean-Frederic Clere <jfclere@gmail.com>
2017-09-21 13:42:15 +0200

    Update set-env.sh
    Install java and maven

--- tomcat-in-the-cloud/deploy-titc-using-cli/set-env.sh
+++ tomcat-in-the-cloud/deploy-titc-using-cli/set-env.sh
@@ -1,5 +1,18 @@
 ~/.launch.sh
-git clone https://github.com/iSma/tomcat-in-the-cloud
+# Install jdk
+yum install java-1.8.0-openjdk-devel.x86_64 -y > /tmp/toto.txt
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
+
+# Installing maven
+wget http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
+tar xvf apache-maven-3.5.0-bin.tar.gz
+mv apache-maven-3.5.0  /usr/local/apache-maven
+export M2_HOME=/usr/local/apache-maven
+export M2=$M2_HOME/bin 
+export PATH=$M2:$PATH
+
+# clone my example
+git clone https://github.com/jfclere/tomcat-in-the-cloud
 cd tomcat-in-the-cloud
 oc login localhost:8443 --username=admin --password=admin --insecure-skip-tls-verify=true
 oc new-project tomcat-in-the-cloud

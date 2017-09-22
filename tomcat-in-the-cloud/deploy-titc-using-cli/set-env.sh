ab3cc1b9c6fabb97ed5d38114b986d7c4d5669fb
Jean-Frederic Clere <jfclere@gmail.com>
2017-09-22 18:06:38 +0200

    Update set-env.sh
    remove scenario from the startup...

--- tomcat-in-the-cloud/deploy-titc-using-cli/set-env.sh
+++ tomcat-in-the-cloud/deploy-titc-using-cli/set-env.sh
@@ -12,9 +12,9 @@ export M2=$M2_HOME/bin
 export PATH=$M2:$PATH
 
 # clone my example
-git clone https://github.com/jfclere/tomcat-in-the-cloud
-cd tomcat-in-the-cloud
-oc login localhost:8443 --username=admin --password=admin --insecure-skip-tls-verify=true
-oc new-project tomcat-in-the-cloud
-oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default -n $(oc project -q)
-mvn fabric8:deploy
+# git clone https://github.com/jfclere/tomcat-in-the-cloud
+# cd tomcat-in-the-cloud
+# oc login localhost:8443 --username=admin --password=admin --insecure-skip-tls-verify=true
+# oc new-project tomcat-in-the-cloud
+# oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default -n $(oc project -q)
+# mvn fabric8:deploy

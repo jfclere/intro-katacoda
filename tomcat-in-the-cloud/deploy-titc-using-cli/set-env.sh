59394a48d0c8cd352a3e8324d993e38c10cddf94
Jean-Frederic Clere <jfclere@gmail.com>
2017-09-25 15:39:26 +0200

    Update set-env.sh

--- tomcat-in-the-cloud/deploy-titc-using-cli/set-env.sh
+++ tomcat-in-the-cloud/deploy-titc-using-cli/set-env.sh
@@ -1,6 +1,6 @@
 ~/.launch.sh
 # Install jdk
-yum install java-1.8.0-openjdk-devel.x86_64 -y > /tmp/toto.txt
+yum install java-1.8.0-openjdk-devel.x86_64 -y
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
 
 # Installing maven
@@ -10,11 +10,3 @@ mv apache-maven-3.5.0  /usr/local/apache-maven
 export M2_HOME=/usr/local/apache-maven
 export M2=$M2_HOME/bin 
 export PATH=$M2:$PATH
-
-# clone my example
-# git clone https://github.com/jfclere/tomcat-in-the-cloud
-# cd tomcat-in-the-cloud
-# oc login localhost:8443 --username=admin --password=admin --insecure-skip-tls-verify=true
-# oc new-project tomcat-in-the-cloud
-# oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default -n $(oc project -q)
-# mvn fabric8:deploy

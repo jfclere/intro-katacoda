e100845a87577104c3ad5e3889b202acb65a0e8d
Jean-Frederic Clere <jfclere@gmail.com>
2017-10-30 17:41:40 +0100

    adjust url for maven

--- tomcat-in-the-cloud/deploy-titc-using-cli/env-init.sh
+++ tomcat-in-the-cloud/deploy-titc-using-cli/env-init.sh
@@ -6,7 +6,7 @@ yum install java-1.8.0-openjdk-devel.x86_64 -y > /tmp/toto.txt
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
 
 # Installing maven
-wget http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
+wget https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.5.0/apache-maven-3.5.0-bin.tar.gz
 tar xvf apache-maven-3.5.0-bin.tar.gz
 mv apache-maven-3.5.0  /usr/local/apache-maven
 export M2_HOME=/usr/local/apache-maven

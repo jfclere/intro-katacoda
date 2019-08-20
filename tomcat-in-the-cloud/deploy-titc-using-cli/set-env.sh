9056c5133148e3c6f2f943cad783835ddbc1c678
Jean-Frederic Clere <jfclere@gmail.com>
2019-08-20 13:22:11 +0200

    Use ASF https mirror.

--- tomcat-in-the-cloud/deploy-titc-using-cli/set-env.sh
+++ tomcat-in-the-cloud/deploy-titc-using-cli/set-env.sh
@@ -4,7 +4,7 @@ yum install java-1.8.0-openjdk-devel.x86_64 -y
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
 
 # Installing maven
-wget https://www.pirbot.com/mirrors/apache/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
+wget https://www-eu.apache.org/dist/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
 tar xvf apache-maven-3.6.1-bin.tar.gz
 mv apache-maven-3.6.1  /usr/local/apache-maven
 export M2_HOME=/usr/local/apache-maven

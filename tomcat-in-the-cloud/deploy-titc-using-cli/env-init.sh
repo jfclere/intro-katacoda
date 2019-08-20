2a6026e45f12fc901cfbb727ce3a57fd4c05ab51
Jean-Frederic Clere <jfclere@gmail.com>
2019-08-20 12:59:17 +0200

    Update maven to the latest version.

--- tomcat-in-the-cloud/deploy-titc-using-cli/env-init.sh
+++ tomcat-in-the-cloud/deploy-titc-using-cli/env-init.sh
@@ -6,9 +6,9 @@ yum install java-1.8.0-openjdk-devel.x86_64 -y > /tmp/toto.txt
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
 
 # Installing maven
-wget https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.5.0/apache-maven-3.5.0-bin.tar.gz
-tar xvf apache-maven-3.5.0-bin.tar.gz
-mv apache-maven-3.5.0  /usr/local/apache-maven
+wget https://www.pirbot.com/mirrors/apache/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
+tar xvf apache-maven-3.6.1-bin.tar.gz
+mv apache-maven-3.6.1  /usr/local/apache-maven
 export M2_HOME=/usr/local/apache-maven
 export M2=$M2_HOME/bin 
 export PATH=$M2:$PATH

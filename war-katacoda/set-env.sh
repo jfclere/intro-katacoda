25f227f3ceabf0644e9e06c8cec997958c6eb9fb
Jean-Frederic Clere <jfclere@gmail.com>
2019-08-21 18:26:26 +0200

    start both master and get node1 to join.

--- war-katacoda/set-env.sh
+++ war-katacoda/set-env.sh
@@ -1,3 +1,4 @@
+if [ $HOSTNAME = "master" ]; then
 kubeadm init --token=102952.1a7dd4cc8d1f4cc5 --kubernetes-version $(kubeadm version -o short)
 
 sudo cp /etc/kubernetes/admin.conf $HOME/
@@ -5,6 +6,16 @@ sudo chown $(id -u):$(id -g) $HOME/admin.conf
 export KUBECONFIG=$HOME/admin.conf
 
 kubectl apply -f /opt/weave-kube
-kubectl get pod -n kube-system
+kubectl get pod -n kube-systemA
+else
+while true do
+  sleep 10
+  curl -v -k https://172.17.0.53:6443
+  if [ $? -eq 0 ]; then
+    kubeadm join --discovery-token-unsafe-skip-ca-verification --token=102952.1a7dd4cc8d1f4cc5 172.17.0.53:6443
+    break
+  fi
+  done
+fi
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre

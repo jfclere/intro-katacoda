76602416c0fed1291f6abbb22f438215ce9470c9
Jean-Frederic Clere <jfclere@gmail.com>
2019-08-21 18:05:40 +0200

    Add starting the master.

--- war-katacoda/set-env.sh
+++ war-katacoda/set-env.sh
@@ -1,2 +1,10 @@
-launch.sh
+kubeadm init --token=102952.1a7dd4cc8d1f4cc5 --kubernetes-version $(kubeadm version -o short)
+
+sudo cp /etc/kubernetes/admin.conf $HOME/
+sudo chown $(id -u):$(id -g) $HOME/admin.conf
+export KUBECONFIG=$HOME/admin.conf
+
+kubectl apply -f /opt/weave-kube
+kubectl get pod -n kube-system
+
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre

ff50740d3639d7dfdf3dfed2f38d8e91ce59c13d
Jean-Frederic Clere <jfclere@gmail.com>
2019-08-22 12:28:20 +0200

    add ansi2txt and fix a typo.

--- war-katacoda/set-env.sh
+++ war-katacoda/set-env.sh
@@ -8,8 +8,10 @@ export KUBECONFIG=$HOME/admin.conf
 kubectl apply -f /opt/weave-kube
 kubectl get pod -n kube-system
 
-MASTERIPPORT=`kubectl cluster-info | grep master | awk ' { print $6 } ' | sed 's.https://..'`
-ssh -o "StrictHostKeyChecking no" node1 kubeadm join --discovery-token-unsafe-skip-ca-verification --token=102952.1a7dd4cc8d1f4cc5 $MASTERIPPORT
+apt install kbtin
+
+MASTERIPPORT=`kubectl cluster-info | grep master | awk ' { print $6 } ' | sed 's.https://..' | ansi2txt`
+ssh -o "StrictHostKeyChecking no" node01 kubeadm join --discovery-token-unsafe-skip-ca-verification --token=102952.1a7dd4cc8d1f4cc5 $MASTERIPPORT
 fi
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre

90c8a72f4a6d3009c3fe4fd90e213074ddc4f092
Jean-Frederic Clere <jfclere@gmail.com>
2019-08-22 10:39:46 +0200

    use ssh to node01

--- war-katacoda/set-env.sh
+++ war-katacoda/set-env.sh
@@ -7,16 +7,9 @@ export KUBECONFIG=$HOME/admin.conf
 
 kubectl apply -f /opt/weave-kube
 kubectl get pod -n kube-system
-else
-  while true
-  do
-    sleep 10
-    curl -v -k https://172.17.0.53:6443
-    if [ $? -eq 0 ]; then
-      kubeadm join --discovery-token-unsafe-skip-ca-verification --token=102952.1a7dd4cc8d1f4cc5 172.17.0.53:6443
-      break
-    fi
-  done
+
+MASTERIPPORT=`kubectl cluster-info | grep master | awk ' { print $6 } ' | sed 's.https://..'
+ssh -o "StrictHostKeyChecking no" node1 kubeadm join --discovery-token-unsafe-skip-ca-verification --token=102952.1a7dd4cc8d1f4cc5 $MASTERIPPORT
 fi
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre

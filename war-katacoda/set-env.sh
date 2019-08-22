80cc3f5f0d771411750d998181d3f2403073d9e6
Jean-Frederic Clere <jfclere@gmail.com>
2019-08-22 10:42:05 +0200

    typo.

--- war-katacoda/set-env.sh
+++ war-katacoda/set-env.sh
@@ -8,7 +8,7 @@ export KUBECONFIG=$HOME/admin.conf
 kubectl apply -f /opt/weave-kube
 kubectl get pod -n kube-system
 
-MASTERIPPORT=`kubectl cluster-info | grep master | awk ' { print $6 } ' | sed 's.https://..'
+MASTERIPPORT=`kubectl cluster-info | grep master | awk ' { print $6 } ' | sed 's.https://..'`
 ssh -o "StrictHostKeyChecking no" node1 kubeadm join --discovery-token-unsafe-skip-ca-verification --token=102952.1a7dd4cc8d1f4cc5 $MASTERIPPORT
 fi
 

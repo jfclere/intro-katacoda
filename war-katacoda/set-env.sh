26d1646b4e03d2755194edf4e89b3647a6786d0f
Jean-Frederic Clere <jfclere@gmail.com>
2020-08-26 11:27:47 +0200

    Update set-env.sh

--- war-katacoda/set-env.sh
+++ war-katacoda/set-env.sh
@@ -1,4 +1,6 @@
 
+apt install kbtin
+
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
 
 if [ $HOSTNAME = "controlplane" ]; then
@@ -11,8 +13,6 @@ export KUBECONFIG=$HOME/admin.conf
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 kubectl get pod -n kube-system
 
-apt install kbtin
-
-CPIPPORT=`kubectl cluster-info | grep controlplane | awk ' { print $6 } ' | sed 's.https://..' | ansi2txt`
+CPIPPORT=`kubectl cluster-info | grep master | awk ' { print $6 } ' | sed 's.https://..' | ansi2txt`
 ssh -o "StrictHostKeyChecking no" node01 kubeadm join --discovery-token-unsafe-skip-ca-verification --token=102952.1a7dd4cc8d1f4cc5 $CPIPPORT
 fi
